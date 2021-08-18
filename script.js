60fb4f2bfc6c930e532ee005aa8b437695b2fa11
azadtamal <mrazadt@gmail.com>
2021-08-18 12:14:17 +0600

    shopping

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function iPhoneCart(e) {
+// function iPhoneCart(e) {
+//   const phoneInput = document.getElementById("phoneInput");
+//   const phoneInputNumber = phoneInput.value;
+
+//   console.log(phoneInputNumber);
+// }
+
+document.getElementById("buttonP").addEventListener("click", () => {
   const phoneInput = document.getElementById("phoneInput");
-}
+  const phoneInputNumber = phoneInput.value;
+  phoneInput.value = parseFloat(phoneInputNumber) + 1;
+});
