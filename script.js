bd822f078414289f0ab6fc79b7710772ead0de59
azadtamal <mrazadt@gmail.com>
2021-08-18 16:48:07 +0600

    function

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-// function iPhoneCart(e) {
-//   const phoneInput = document.getElementById("phoneInput");
-//   const phoneInputNumber = phoneInput.value;
-
-//   console.log(phoneInputNumber);
-// }
-
-document.getElementById("buttonP").addEventListener("click", () => {
+function iPhoneCart(e) {
   const phoneInput = document.getElementById("phoneInput");
   const phoneInputNumber = phoneInput.value;
-  phoneInput.value = parseFloat(phoneInputNumber) + 1;
+  if (e == true) {
+    phoneInput.value = parseFloat(phoneInputNumber) + 1;
+  } else if (phoneInputNumber > 0) {
+    phoneInput.value = parseFloat(phoneInputNumber) - 1;
+  }
+  const PhoneAmount = document.getElementById("PhoneAmount");
+  PhoneAmount.innerText = phoneInputNumber * 1219;
+}
+
+document.getElementById("buttonP").addEventListener("click", () => {
+  iPhoneCart(true);
+});
+document.getElementById("BtnMinus").addEventListener("click", () => {
+  iPhoneCart(false);
 });
